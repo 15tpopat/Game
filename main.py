86ebd80d3dab53f3de9936940523cc96193de1d2
15tpopat <97695260+15tpopat@users.noreply.github.com>
2022-02-08 17:59:46 +0000

    Add game loop, pygame functions and comments

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import pygame
+import sys
 
 if __name__ == "__main__":
+    # Initialise pygame
     pygame.init()
     pygame.display.set_caption("Game")
+
+    # Manage how often the screen updates
+    clock = pygame.time.Clock()
+
+    # Initiate the screen with the given width and height
     screen = pygame.display.set_mode([600, 400])
-    pygame.quit()
+
+    # Start game loop
+    while True:
+        # Event processing
+        for event in pygame.event.get():
+            # If they click the exit button, quit pygame and exit python
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Update the screen
+        pygame.display.update()
+
+        # Limit the screen updates to 60 frames per second
+        clock.tick(60)
