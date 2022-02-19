21e5ca4e21b4fbc65851366e61df3f0c6e48a980
15tpopat <97695260+15tpopat@users.noreply.github.com>
2022-02-19 20:59:13 +0000

    Allow for manual change in colour

--- logging.py
+++ logging.py
@@ -1,24 +1,18 @@
 from colorama import Fore
 from inspect import getframeinfo, stack
 from os import getcwd
-from typing import Optional
 
 ROOT_FOLDER = getcwd().split("/")[-1] # Get the tail folder of the working directory
 ROOT_FOLDER = "/" + ROOT_FOLDER + "/" # (The parent folder of the server.py and main.py files)
 
-def colourPrint(text: str, colour: Optional[str] = None, end: str = "\n"):
+def colourPrint(text: str, colour: str = "reset", end: str = "\n"):
     if colour is None:
         raise TypeError("'colour' parameter cannot be NoneType.")
 
     print(eval("Fore.{}".format(colour.upper())) + text, end=end)
     print("\033[39m", end="")
 
-def message(*text: str):
-    text = " ".join(text)
-    colourPrint(text + ".", colour="cyan")
-
-def infoMessage(*text: str, start: str = ""):
-    text = " ".join(text)
+def infoMessage(text: str, start: str = "", colour: str = "cyan"):
     caller = getframeinfo(stack()[1][0])
     fileName = caller.filename[caller.filename.rindex(ROOT_FOLDER):]
     colourPrint("{start}{filename}:{lineNumber}\t\t{text}".format(
@@ -26,10 +20,9 @@ def infoMessage(*text: str, start: str = ""):
                 filename=fileName,
                 lineNumber=caller.lineno,
                 text=text + "."),
-                colour="cyan")
+                colour=colour)
 
-def debugMessage(*text: str, start: str = ""):
-    text = " ".join(text)
+def debugMessage(text: str, start: str = "", colour: str = "magneta"):
     caller = getframeinfo(stack()[1][0])
     fileName = caller.filename[caller.filename.rindex(ROOT_FOLDER):]
     colourPrint("{start}{filename}:{lineNumber}\t\t{text}".format(
@@ -37,14 +30,13 @@ def debugMessage(*text: str, start: str = ""):
                 filename=fileName,
                 lineNumber=caller.lineno,
                 text=text + "..."),
-                colour="magenta")
+                colour=colour)
 
-def errorMessage(*text: str):
-    text = " ".join(text)
+def errorMessage(text: str, colour: str = "red"):
     caller = getframeinfo(stack()[1][0])
     fileName = caller.filename[caller.filename.rindex(ROOT_FOLDER):]
     colourPrint("{filename}:{lineNumber}\t\t{text}".format(
                 filename=fileName,
                 lineNumber=caller.lineno,
                 text=text + "!"),
-                colour="red")
+                colour=colour)
