dbd42fa8f3795687930bb4c82db55ebdc8e6f357
15tpopat <97695260+15tpopat@users.noreply.github.com>
2022-03-06 16:14:16 +0000

    Fix filename and add line number digit conformity

--- logging.py
+++ logging.py
@@ -1,9 +1,17 @@
 from colorama import Fore
-from inspect import getframeinfo, stack
+from inspect import getframeinfo, stack, Traceback
 from os import getcwd
 
 ROOT_FOLDER = getcwd().split("/")[-1] # Get the tail folder of the working directory
 ROOT_FOLDER = "/" + ROOT_FOLDER + "/" # (The parent folder of the server.py and main.py files)
+
+def getFilename(caller: Traceback) -> str:
+    if ROOT_FOLDER in caller.filename:
+        filename = caller.filename[caller.filename.rindex(ROOT_FOLDER) + len(ROOT_FOLDER):]
+    else:
+        filename = caller.filename
+
+    return filename
 
 def colourPrint(text: str, colour: str = "reset", end: str = "\n"):
     if colour is None:
@@ -14,33 +22,33 @@ def colourPrint(text: str, colour: str = "reset", end: str = "\n"):
 
 def infoMessage(text: str, start: str = "", colour: str = "blue", end: str = ".\n"):
     caller = getframeinfo(stack()[1][0])
-    fileName = caller.filename[caller.filename.rindex(ROOT_FOLDER):]
+    filename = getFilename(caller)
     colourPrint("{start}{filename}:{lineNumber}\t\t{text}".format(
                 start=start,
-                filename=fileName,
-                lineNumber=caller.lineno,
+                filename=filename,
+                lineNumber=f"{caller.lineno:03d}", # Ensures the line number has the same number of digits
                 text=text),
                 colour=colour,
                 end=end)
 
 def debugMessage(text: str, start: str = "", colour: str = "magneta", end: str = "...\n"):
     caller = getframeinfo(stack()[1][0])
-    fileName = caller.filename[caller.filename.rindex(ROOT_FOLDER):]
+    filename = getFilename(caller)
     colourPrint("{start}{filename}:{lineNumber}\t\t{text}".format(
                 start=start,
-                filename=fileName,
-                lineNumber=caller.lineno,
+                filename=filename,
+                lineNumber=f"{caller.lineno:03d}", # Ensures the line number has the same number of digits
                 text=text),
                 colour=colour,
                 end=end)
 
 def errorMessage(text: str, colour: str = "red", prefix: bool = True, end: str = "!\n"):
     caller = getframeinfo(stack()[1][0])
-    fileName = caller.filename[caller.filename.rindex(ROOT_FOLDER):]
+    filename = getFilename(caller)
     if prefix:
         colourPrint("{filename}:{lineNumber}\t\t{text}".format(
-                    filename=fileName,
-                    lineNumber=caller.lineno,
+                    filename=filename,
+                    lineNumber=f"{caller.lineno:03d}", # Ensures the line number has the same number of digits
                     text=text),
                     colour=colour,
                     end=end)
