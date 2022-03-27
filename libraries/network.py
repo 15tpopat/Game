dc288c901c35dd25c3961cafc9f19f51e26aff7b
15tpopat <97695260+15tpopat@users.noreply.github.com>
2022-03-27 16:48:54 +0100

    Add server and network comments

--- libraries/network.py
+++ libraries/network.py
@@ -10,22 +10,32 @@ class Network:
     """ This class represents the the client interface to the server. """
 
     def __init__(self) -> object:
+        # Create an IPV4 TCP socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Set the network attributes
         self.host = HOST
         self.port = PORT
         self.addr = (self.host, self.port)
+
+        # Connect to the server and receive the initial player object
         self.player = self.connect()
 
     def connect(self) -> object:
         try:
+            # Try and connect to the server
             self.socket.connect(self.addr)
+
+            # Receive the initial player object
             return pLoads(self.socket.recv(DATA_SIZE))
         except socket.error as e:
+            # If an error occurs, it likely means that the server is not listening for connections
             errorMessage("An error occurred whilst trying to connect to the server", end=": ")
             errorMessage(str(e), prefix=False)
             infoMessage("The server is most likely not started")
             sys.exit()
 
     def send(self, data: dict) -> dict:
+        # Send the data to the server and receive data back
         self.socket.send(pDumps(data))
         return pLoads(self.socket.recv(DATA_SIZE))
--- server.py
+++ server.py
@@ -15,29 +15,40 @@ class Server:
     """ This class represents the server. """
 
     def __init__(self) -> object:
+        # Create an IPV4 TCP socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Set the network attributes
         self.host = HOST
         self.port = PORT
         self.addr = (self.host, self.port)
+
+        # Set the game tracking attributes
         self.playerIndex = 0
 
     def start(self) -> None:
+        # Try to create a socket to an unbinded address
+        # If the address is already binded, an error will occur
         try:
             self.socket.bind(self.addr)
         except Exception as e:
             errorMessage("Error trying to bind server", end=": ")
             errorMessage(str(e), prefix=False)
 
+        # Only allow for a certain number of connections
         self.socket.listen(MAXIMUM_NUM_CONNECTIONS)
         infoMessage(f"Listening for connections on {self.port}")
         self.listenForConnections()
 
     def listenForConnections(self) -> None:
         while True:
+            # Accept incoming connections and create a new thread for each connection
             conn, clientAddress = self.socket.accept()
             infoMessage(f"Connection from {clientAddress}")
             clientThread = Thread(target=self.clientThread, args=(conn, clientAddress, self.playerIndex))
             clientThread.start()
+
+            # Increment the player ID
             self.playerIndex += 1
 
     def clientThread(self, conn: socket.socket, clientAddress: tuple, playerIndex: int) -> None:
@@ -90,17 +101,21 @@ class Server:
                         else:
                             jutsu[jutsuObject.jutsuID] = jutsuObject
                 else:
+                    # If no data is received, the client has disconnected
                     infoMessage(f"{clientAddress} has disconnected", colour="yellow")
                     connected = False
                     conn.sendall(pDumps([None, None]))
 
+                # Send the data back to the client
                 conn.sendall(pDumps([playerList, jutsu]))
 
             except Exception as e:
+                # If we can't send data to the client, the client has disconnected
                 errorMessage(f"{clientAddress} has disconnected", end=": ")
                 errorMessage(str(e), prefix=False)
                 connected = False
 
+        # Decrement the player ID and delete the respective player object
         infoMessage(f"Ended threaded tasks for client: {clientAddress}")
         self.playerIndex -= 1
         del players[playerIndex]
